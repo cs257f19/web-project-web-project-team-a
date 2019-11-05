f659296055a6618030ed655f257258b07d336de7
Tony Ngo <tonyngo@eduroam-165-134.dyn.carleton.edu>
2019-11-05 14:55:46 -0600

    changes

--- prototypedatasource.py
+++ prototypedatasource.py
@@ -37,10 +37,10 @@ class courseQuery:
 		print(courses)
 		for course in courses:
 			print("hit8")
-			i=0
-			for len(course) in course:
+			
+			for i in len(course):
 				print(course[i])
-				i++
+				
 			courseObj = CourseObj(course)
 			print("hit9")
 			courseResults.append(courseObj)
