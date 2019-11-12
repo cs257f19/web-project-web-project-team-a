4381e2d64a07144791c3472b0ec7e0d4921fe262
Tony Ngo <tonyngo@eduroam-165-234.dyn.carleton.edu>
2019-11-12 16:43:21 -0600

    edits

--- webapp.py
+++ webapp.py
@@ -18,8 +18,7 @@ def hello():
 def searchResult():
 	if request.method == 'POST':
 		result = request.form
-		ds = courseQuery.datasource(None, None, result.get("search"), None, None, None)
-		#ds.close()
+		ds = datasouce.DataSource(None, None, result.get("search"), None, None, None)
 		description = "Showing all classes that have  " + result.get("search") + " sorted alphabetically"
 		result = ds.getCourseName()
 		return render_template('result.html', result = result, description = description)
