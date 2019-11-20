1512eed2194ef3e349000fdb349d68ff8b2f683b
Tony Ngo <tonyngo@eduroam-165-234.dyn.carleton.edu>
2019-11-19 19:32:59 -0600

    implement masterquery

--- datasource.py
+++ datasource.py
@@ -183,7 +183,7 @@ class CourseQuery:
 			query = self.courseTerm[0]
 			if len(self.courseTerm) > 1:
 				for termIndex in range(1, len(self.courseTerm)):
-					query = query + " UNION "+ self.getCourseByTermHelper(self.course[termIndex])
+					query = query + " UNION "+ self.getCourseByTermHelper(self.courseTerm[termIndex])
 			return query
 		else:
 			return None
